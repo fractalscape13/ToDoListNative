d4e8468799e66b49e746879c74316a7a5d7bb31c
Joseph Wangemann <fractalscape13@gmail.com>
2020-06-04 10:47:08 -0700

    add contact component

---
+++ components/Contact.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Contact() {
+
+  return (
+    <View style={styles.contact}>
+      <Text style={styles.text}> Email us with any questions</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  contact: {
+    flex: 1,
+    backgroundColor: 'rgb(120, 150, 140)',
+    width: '80%',
+    borderTopColor: 'ivory',
+    borderStyle: 'solid',
+    borderTopWidth: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    color: 'ivory',
+    margin: 8,
+  }
+});
